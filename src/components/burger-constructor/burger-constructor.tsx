791f6fdcd2d21af55e719c62f4c2d9f3cedd2357
Alexander Semenov <sasfeat@mail.ru>
2022-08-08 23:32:16 +0300

    Make BurgerConstructor functional component

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -8,66 +8,64 @@ interface Props {
   bun: IngredientInterface;
 }
 
-export class BurgerConstructor extends React.Component<Props> {
-  render() {
-    const wrapperClassName = `${styles.constructor}`;
-    const totalClassName = `${styles.total} mt-10`;
-    const totalPriceClassName = `${styles.totalPrice} text text_type_digits-medium mr-10`;
-    const draggableItemClassName = `${styles.draggableItem}`;
-    const constructorDynamicClassName = `${styles.constructorDynamic} pr-2`;
-    return (
-      <>
-        <div className={wrapperClassName}>
-          {this.props.bun && (
-            <div className="ml-8 pr-4">
-              <ConstructorElement
-                type="top"
-                isLocked={true}
-                text={`${this.props.bun.name} (верх)`}
-                price={this.props.bun.price}
-                thumbnail={this.props.bun.image}
-              />
-            </div>
-          )}
-          <div className={constructorDynamicClassName}>
-            {this.props.ingredients.map((ingredient, ix) => (
-              <div
-                className={draggableItemClassName}
-                key={ingredient._id}>
-                <DragIcon type="primary" />
-                <ConstructorElement
-                  text={ingredient.name}
-                  price={ingredient.price}
-                  thumbnail={ingredient.image}
-                />
-              </div>
-            ))}
+export const BurgerConstructor = (props: Props) => {
+  const wrapperClassName = `${styles.constructor}`;
+  const totalClassName = `${styles.total} mt-10`;
+  const totalPriceClassName = `${styles.totalPrice} text text_type_digits-medium mr-10`;
+  const draggableItemClassName = `${styles.draggableItem}`;
+  const constructorDynamicClassName = `${styles.constructorDynamic} pr-2`;
+  return (
+    <>
+      <div className={wrapperClassName}>
+        {props.bun && (
+          <div className="ml-8 pr-4">
+            <ConstructorElement
+              type="top"
+              isLocked={true}
+              text={`${props.bun.name} (верх)`}
+              price={props.bun.price}
+              thumbnail={props.bun.image}
+            />
           </div>
-
-          {this.props.bun && (
-            <div className="ml-8 pr-4">
+        )}
+        <div className={constructorDynamicClassName}>
+          {props.ingredients.map((ingredient, ix) => (
+            <div
+              className={draggableItemClassName}
+              key={ingredient._id}>
+              <DragIcon type="primary" />
               <ConstructorElement
-                type="bottom"
-                isLocked={true}
-                text={`${this.props.bun.name} (низ)`}
-                price={this.props.bun.price}
-                thumbnail={this.props.bun.image}
+                text={ingredient.name}
+                price={ingredient.price}
+                thumbnail={ingredient.image}
               />
             </div>
-          )}
+          ))}
         </div>
-        <div className={totalClassName}>
-          <p className={totalPriceClassName}>
-            610
-            <CurrencyIcon type="primary" />
-          </p>
-          <Button
-            type="primary"
-            size="large">
-            Оформить заказ
-          </Button>
-        </div>
-      </>
-    );
-  }
-}
+
+        {props.bun && (
+          <div className="ml-8 pr-4">
+            <ConstructorElement
+              type="bottom"
+              isLocked={true}
+              text={`${props.bun.name} (низ)`}
+              price={props.bun.price}
+              thumbnail={props.bun.image}
+            />
+          </div>
+        )}
+      </div>
+      <div className={totalClassName}>
+        <p className={totalPriceClassName}>
+          610
+          <CurrencyIcon type="primary" />
+        </p>
+        <Button
+          type="primary"
+          size="large">
+          Оформить заказ
+        </Button>
+      </div>
+    </>
+  );
+};
