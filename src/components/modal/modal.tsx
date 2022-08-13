5aee01d2579fbe8bc20aa28d9cf3c2fd6755a93c
Alexander Semenov <sasfeat@mail.ru>
2022-08-13 13:58:05 +0300

    Close modal on Escape

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -2,7 +2,7 @@ import { createPortal } from 'react-dom';
 import styles from './modal.module.css';
 import { HeaderModal } from './modal-header/header-modal';
 import { ModalBackdrop } from './modal-backdrop/modal-backdrop';
-import { MouseEventHandler } from 'react';
+import { MouseEventHandler, SyntheticEvent, useEffect } from 'react';
 import React from 'react';
 
 const modalRoot = document.getElementById('modals')!;
@@ -14,6 +14,18 @@ interface ModalProps {
 }
 export function Modal(props: ModalProps) {
   const modalClassName = `${styles.modal}`;
+
+  useEffect(() => {
+    const close = (e: KeyboardEvent) => {
+      if (e.code === 'Escape') {
+        // @ts-ignore
+        props.onClose(e);
+      }
+    };
+    window.addEventListener('keydown', close);
+    return () => window.removeEventListener('keydown', close);
+  }, []);
+
   return createPortal(
     <>
       <div className={modalClassName}>
